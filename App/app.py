68b00f09f8a9adeaf00700001164daaa9f994ee6
ricarsant <ricarsant@hotmail.com>
2020-08-17 14:09:40 -0500

    version1

---
+++ App/app.py
@@ -0,0 +1,218 @@
+"""
+ * Copyright 2020, Departamento de sistemas y Computación, Universidad de Los Andes
+ * 
+ * Contribución de:
+ *
+ * Cristian Camilo Castellanos
+ *
+ * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ """
+
+"""
+  Este módulo es una aplicación básica con un menú de opciones para cargar datos, contar elementos, y hacer búsquedas sobre una lista.
+"""
+
+import config as cf
+import sys
+import csv
+from time import process_time 
+import os
+
+def loadCSVFile (file, lst, sep=";"):
+    print("Archivo... ",file)
+    print(os.getcwd())
+    print(os.path.join(os.getcwd(), file))
+    file = os.path.join(os.getcwd(), file)
+    print("file " , file)
+    """
+    Carga un archivo csv a una lista
+    Args:
+        file 
+            Archivo de texto del cual se cargaran los datos requeridos.
+        lst :: []
+            Lista a la cual quedaran cargados los elementos despues de la lectura del archivo.
+        sep :: str
+            Separador escodigo para diferenciar a los distintos elementos dentro del archivo.
+    Try:
+        Intenta cargar el archivo CSV a la lista que se le pasa por parametro, si encuentra algun error
+        Borra la lista e informa al usuario
+    Returns: None   
+    """
+    del lst[:]
+    print("Cargando archivo ....")
+    t1_start = process_time() #tiempo inicial
+    dialect = csv.excel()
+    dialect.delimiter=sep
+    try:
+        with open(file, encoding="utf-8") as csvfile:
+           
+            spamreader = csv.DictReader(csvfile, dialect=dialect)
+            for row in spamreader: 
+                #print(" row...", row )
+                lst.append(row)
+    except:
+        del lst[:]
+        print("Se presento un error en la carga del archivo")
+    
+    t1_stop = process_time() #tiempo final
+    print("Tiempo de ejecución ",t1_stop-t1_start," segundos")
+
+def printMenu():
+    """
+    Imprime el menu de opciones
+    """
+    print("\nBienvenido")
+    print("1- Cargar Datos")
+    print("2- Contar los elementos de la Lista")
+    print("3- Contar elementos filtrados por palabra clave")
+    print("4- Consultar elementos a partir de filtro")
+    print("5- Consultar elementos a partir de dos listas")
+    print("0- Salir")
+
+def countElementsFilteredByColumn(criteria, column, lst):
+    """
+    Retorna cuantos elementos coinciden con un criterio para una columna dada  
+    Args:
+        criteria:: str
+            Critero sobre el cual se va a contar la cantidad de apariciones
+        column
+            Columna del arreglo sobre la cual se debe realizar el conteo
+        list
+            Lista en la cual se realizará el conteo, debe estar inicializada
+    Return:
+        counter :: int
+            la cantidad de veces ue aparece un elemento con el criterio definido
+    """
+    if len(lst)==0:
+        print("La lista esta vacía")  
+        return 0
+    else:
+        t1_start = process_time() #tiempo inicial
+        counter=0 #Cantidad de repeticiones
+        for element in lst:
+            if criteria.lower() in element[column].lower(): #filtrar por palabra clave 
+                counter+=1
+        t1_stop = process_time() #tiempo final
+        print("Tiempo de ejecución ",t1_stop-t1_start," segundos")
+    return counter
+
+def countElementsByCriteria(criteria, column, lst):
+    """
+    Retorna la cantidad de elementos que cumplen con un criterio para una columna dada
+    """
+    if len(lst)==0:
+        print("La lista esta vacía")  
+        return 0
+    else:
+        t1_start = process_time() #tiempo inicial
+        counter=0 #Cantidad de repeticiones
+        for element in lst:
+            if element[column] >= criteria : #filtrar por palabra clave 
+                counter+=1
+        t1_stop = process_time() #tiempo final
+        print("Tiempo de ejecución ",t1_stop-t1_start," segundos")
+    return counter
+
+
+def countElementsByCriteria1(criteria, column, lst):
+    """
+    Retorna la cantidad de elementos que cumplen con un criterio para una columna dada
+    """
+    lista2 = []
+
+    promedio =[]
+
+
+    loadCSVFile("Data\\AllMoviesCastingRaw.csv", lista2)
+    print("len lista2 ", len(lista2))
+    print("len lst  ", len(lst))
+
+    '''
+    for el1 in lst:
+        print("solo elemento ", el1)
+        print("Primer elemento ", el1['\ufeffid'])
+        break
+    '''
+
+    
+    if len(lst)==0:
+        print("La lista esta vacía")  
+        return 0
+    else:
+        t1_start = process_time() #tiempo inicial
+        #counter=0 #Cantidad de repeticiones
+        for element in lista2: #encontarar los id de peliculas por director
+            if criteria.lower() in element[column].lower(): #filtrar por palabra clave 
+                id1 = element['id']
+                #print("iddddd 1 ..", id1)
+
+                for elemento1 in lst:    #encontarar vote_average las peliculas del director
+                    if elemento1['\ufeffid'] == id1:
+                        if float(elemento1["vote_average"])>= 6:
+                            promedio.append(float(elemento1["vote_average"]))
+                            break
+
+
+
+
+
+        t1_stop = process_time() #tiempo final
+        print("Tiempo de ejecución ",t1_stop-t1_start," segundos")
+    return promedio
+    
+
+def main():
+    """
+    Método principal del programa, se encarga de manejar todos los metodos adicionales creados
+
+    Instancia una lista vacia en la cual se guardarán los datos cargados desde el archivo
+    Args: None
+    Return: None 
+    """
+    lista = [] #instanciar una lista vacia
+    while True:
+        printMenu() #imprimir el menu de opciones en consola
+        inputs =input('Seleccione una opción para continuar\n') #leer opción ingresada
+        if len(inputs)>0:
+            if int(inputs[0])==1: #opcion 1
+                loadCSVFile("Data\\AllMoviesDetailsCleaned.csv", lista) #llamar funcion cargar datos
+                print("Datos cargados, "+str(len(lista))+" elementos cargados")
+            elif int(inputs[0])==2: #opcion 2
+                if len(lista)==0: #obtener la longitud de la lista
+                    print("La lista esta vacía")    
+                else: print("La lista tiene "+str(len(lista))+" elementos")
+            elif int(inputs[0])==3: #opcion 3
+                criteria =input('Ingrese el criterio3 de búsqueda\n')
+                counter=countElementsFilteredByColumn(criteria, "production_countries", lista) #filtrar una columna por criterio  
+                print("Coinciden ",counter," elementos con el crtierio: ", criteria  )
+            elif int(inputs[0])==4: #opcion 4
+                criteria =input('Ingrese el criterio de búsqueda\n')
+                counter=countElementsByCriteria(criteria,"vote_average",lista)
+                print("Coinciden ",counter," elementos con el crtierio: '", criteria ,"'")
+
+            elif int(inputs[0])==5: #opcion 5
+                criteria =input('Ingrese el criterio de búsqueda\n')
+                counter=countElementsByCriteria1(criteria,"director_name",lista)
+                print("Coinciden ",len(counter)," elementos con el crtierio buenas peliculas director: '", criteria ,"'")
+                print("Promedio buenas peliculas director: ", criteria , sum(counter)/len(counter))
+ 
+
+            elif int(inputs[0])==0: #opcion 0, salir
+                sys.exit(0)
+
+if __name__ == "__main__":
+    main()
